7abc12596cbc8e5aa522f3391e3efb65e51294b1
ufdevs <ramesh_25s12res130@iitp.ac.in>
2025-05-21 21:46:21 +0530

    mongo fix

--- server/app.js
+++ server/app.js
@@ -88,10 +88,10 @@ app.use((err, req, res, next) => {
 });
 
 // app.listen(3000);// ✅ Only start server if not on Vercel
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-  }
+// if (process.env.NODE_ENV !== 'production') {
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Server running on http://localhost:${PORT}`);
+//     });
+//   }
 module.exports = app;
