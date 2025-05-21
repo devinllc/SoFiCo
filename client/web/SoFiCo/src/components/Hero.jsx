578963664b1af93462d3a1bcfefd2615ec5b5281
Abhishek Raj <abhishekraj995555@gmail.com>
2025-05-21 20:00:26 +0530

    updated now

--- client/web/SoFiCo/src/components/Hero.jsx
+++ client/web/SoFiCo/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { MdArrowOutward } from "react-icons/md";
 
 export default function Hero() {
     return (
-      <section className="flex flex-col md:flex-row items-center justify-center gap-60 bg-gradient-to-br from-[#0e2d3c] to-[#116466] min-h-[80vh]">
+      <section className="flex flex-col md:flex-row items-center justify-center gap-60 bg-gradient-to-br from-[#0e2d3c] to-[#116466] min-h-[100vh]">
         <div className="max-w-xl">
           <span className="bg-gray-800 text-xs rounded-full px-3 py-1 mb-4 inline-block text-green-300 font-semibold">🔥 100% TRUSTED PLATFORM</span>
           <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-4 leading-tight">
