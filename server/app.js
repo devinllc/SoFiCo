db94671a16b746d010fed8472916ff2bacea57af
Ramesh Vishwakarma <114978317+devinllc@users.noreply.github.com>
2025-05-22 18:01:50 +0530

    Cora free

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Configure CORS
 app.use(cors({
-    origin:['http://localhost:3000','http://localhost:3001','http://localhost:5173','http://localhost:5174'],
+    origin:['*'],
     credentials: true,
  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
 
